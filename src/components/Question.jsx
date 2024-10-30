5d3d3070514ec203cf5edcb3287031733814a71f
ashishcheenikal <ashishcheenikal@gmail.com>
2024-10-31 04:40:53 +0530

    Question page UI - 1

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -39,31 +39,47 @@ const Question = () => {
 
   return (
     <div className="h-full flex flex-col justify-center items-center p-4 text-white">
-      <div className="bg-white p-6 rounded-xl shadow-md w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
-        <div className="flex justify-center items-center mb-4">
-          <h2 className="text-2xl sm:text-3xl font-semibold">Quiz Question</h2>
+      <div className="bg-white h-[80%] sm:h-[90%] overflow-y-auto  p-2 sm:p-4 md:p-6 rounded-lg shadow-md w-full sm:w-4/5 md:w-3/5 lg:w-2/5">
+        <div className="h-1/3  flex justify-center items-center mb-4 bg-hp-primary-100 overflow-y-auto">
+          <p className="text-lg text-hp-primary-950 text-center ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+            fuga nulla, magnam hic dolorem quisquam corrupti eius, mollitia
+            perspiciatis, dolor
+          </p>
         </div>
-        <div className="flex justify-between items-center mx-2 my-2">
-          {/* Timer */}
+
+        <div className="flex items-center space-x-4 my-8">
+          <span className="text-orange-600 font-semibold">
+            <span className="text-black font-semibold">Time</span> : {timeLeft}
+          </span>
+
+          <div className="relative flex-grow h-4 bg-orange-100 rounded-full overflow-hidden shadow-inner">
+            <div
+              className="absolute top-0 left-0 h-full bg-gradient-to-r from-yellow-400 to-orange-500 rounded-full"
+              style={{ width: `${points}%` }}
+            ></div>
+          </div>
+          <span className="text-orange-600 font-semibold">
+            <span className="text-black font-semibold">Points</span> : {points}
+          </span>
+        </div>
+
+        {/* <div className="flex flex-col justify-between items-center mx-2 my-2">
           <div className="bg-gray-700 px-4 py-2 rounded-lg text-white text-lg">
             {timeLeft} seconds left
           </div>
           <div className="bg-gray-700 px-4 py-2 rounded-lg text-white text-lg">
             {points} points
           </div>
-        </div>
+        </div> */}
 
-        <p className="mb-6 text-lg text-gray-300 text-center">
-          What is the capital of France?
-        </p>
-
-        <form className="space-y-4" onSubmit={handleAnswerSubmit}>
+        <form className="" onSubmit={handleAnswerSubmit}>
           {/* Answer 1 */}
           <label
-            className={`block p-3 rounded-lg transition-colors cursor-pointer ${
+            className={`block p-3 rounded-lg transition-colors cursor-pointer border border-hp-primary-950 mb-3  ${
               selectedAnswer === 'Berlin'
-                ? 'bg-indigo-600 text-white'
-                : 'bg-gray-800 hover:bg-gray-700'
+                ? 'bg-hp-primary-600 text-hp-primary-950'
+                : 'bg-white text-hp-primary-950 hover:bg-hp-primary-950 hover:text-white'
             }`}
           >
             <input
@@ -79,10 +95,10 @@ const Question = () => {
 
           {/* Answer 2 */}
           <label
-            className={`block p-3 rounded-lg transition-colors cursor-pointer ${
+            className={`block p-3 rounded-lg transition-colors border border-hp-primary-950 cursor-pointer mb-3 ${
               selectedAnswer === 'Madrid'
-                ? 'bg-indigo-600 text-white'
-                : 'bg-gray-800 hover:bg-gray-700'
+                ? 'bg-hp-primary-600 text-hp-primary-950'
+                : 'bg-white text-hp-primary-950 hover:bg-hp-primary-950 hover:text-white'
             }`}
           >
             <input
@@ -98,10 +114,10 @@ const Question = () => {
 
           {/* Answer 3 */}
           <label
-            className={`block p-3 rounded-lg transition-colors cursor-pointer ${
+            className={`block p-3 rounded-lg transition-colors cursor-pointer border border-hp-primary-950 mb-3 ${
               selectedAnswer === 'Paris'
-                ? 'bg-indigo-600 text-white'
-                : 'bg-gray-800 hover:bg-gray-700'
+                ? 'bg-hp-primary-600 text-hp-primary-950'
+                : 'bg-white text-hp-primary-950 hover:bg-hp-primary-950 hover:text-white'
             }`}
           >
             <input
@@ -117,10 +133,10 @@ const Question = () => {
 
           {/* Answer 4 */}
           <label
-            className={`block p-3 rounded-lg transition-colors cursor-pointer ${
+            className={`block p-3 rounded-lg transition-colors cursor-pointer border border-hp-primary-950 mb-3 ${
               selectedAnswer === 'Rome'
-                ? 'bg-indigo-600 text-white'
-                : 'bg-gray-800 hover:bg-gray-700'
+                ? 'bg-hp-primary-600 text-hp-primary-950'
+                : 'bg-white text-hp-primary-950 hover:bg-hp-primary-950 hover:text-white'
             }`}
           >
             <input
@@ -136,7 +152,7 @@ const Question = () => {
 
           <button
             type="submit"
-            className="w-full bg-gradient-to-r from-hp_bg to-blue-400 hover:from-bg-hp_bg via hover:to-blue-500 py-2 rounded-lg text-white font-semibold hover:bg-indigo-700 transition-colors"
+            className="w-full bg-gradient-to-r from-hp_bg to-blue-400 hover:from-bg-hp_bg via hover:to-blue-500 py-2 rounded-lg text-white font-semibold hover:bg-indigo-700 transition-colors mt-5"
           >
             Submit Answer
           </button>
